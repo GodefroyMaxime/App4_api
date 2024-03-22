6e84298a1075e5e608555154eb68a9eaabd7c1fc
GodefroyMaxime <maxime.godefroy@lyreco.com>
2024-03-22 16:04:22 +0100

    [UPDATE] Migration & CRUD

---
+++ src/Form/EmployeeType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employee;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmployeeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname')
+            ->add('employee_id')
+            ->add('active')
+            ->add('employee_id_WD')
+            ->add('pref_lastname')
+            ->add('pref_firstname')
+            ->add('lastname')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Employee::class,
+        ]);
+    }
+}
