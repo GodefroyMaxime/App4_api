07f92e0eb8a3bf85d447bf4d016fb9d1fb44ed72
GodefroyMaxime <maxime.godefroy@lyreco.com>
2024-03-25 16:49:03 +0100

    [UPDATE] Api to BDD

--- src/Service/api/WorkdayService.php
+++ src/Service/api/WorkdayService.php
@@ -2,19 +2,40 @@
 
 namespace App\Service;
 
+use App\Entity\Employee;
 use App\Service\ApiAuthenticationService;
+use Doctrine\ORM\EntityManagerInterface;
 
 class WorkdayService
 {
-    public function __construct(private ApiAuthenticationService $api)
+    public function __construct(private ApiAuthenticationService $authentication, private EntityManagerInterface $entityManager)
     {
     }
 
-    public function test() {
+    public function jsonDecodeReport(string $report) {
         
-        $decodedResponse = json_decode($this->api->fetchData('WD', 'TA_-_WB_-_Employee_List')->getContent());
+        $decodedResponse = json_decode($this->authentication->fetchData('WD', $report)->getContent());
         $reportEntries = $decodedResponse->Report_Entry;
 
-        dd($reportEntries);
+        return $reportEntries;
+    }
+
+    public function EmployeeData() {
+        
+        $data = $this->jsonDecodeReport('TA_-_WB_-_Employee_List');
+        
+        foreach ($data as $key => $value) {
+            $employee = new Employee();
+            $employee->setFirstname($value->firstname);
+            $employee->setEmployeeId((int) $value->employee_id);
+            $employee->setActive($value->active === '1' ? true : false);
+            $employee->setEmployeeIdWD($value->employee_id_WD);
+            $employee->setPrefLastname($value->pref_lastname);
+            $employee->setPrefFirstname($value->pref_firstname);
+            $employee->setLastname($value->lastname);
+            $this->entityManager->persist($employee);
+        }
+
+        $this->entityManager->flush();
     }
 }
